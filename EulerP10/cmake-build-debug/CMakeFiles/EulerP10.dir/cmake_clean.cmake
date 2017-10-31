3e0baac4e060d2e7ff750c499085a96d5543e80d
Rupraj <s521059@mail.nwmissouri.edu>
2017-10-31 17:56:23 -0500

    Add files via upload

---
+++ EulerP10/cmake-build-debug/CMakeFiles/EulerP10.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EulerP10.dir/main.cpp.obj"
+  "EulerP10.pdb"
+  "EulerP10.exe"
+  "EulerP10.exe.manifest"
+  "libEulerP10.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EulerP10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ EulerP10/main.cpp
@@ -0,0 +1,34 @@
+
+# include <conio.h>
+# include <iostream>
+# include <stdio.h>
+
+using namespace std;
+
+long long arr[2000000],i,j;
+long long suma;
+
+int main (){
+    for(i=1; i<2000000;i++)
+        arr[i]=0;
+    i=2; arr[0]=1;
+    arr[1]=1;
+    while (i*i<2000000){
+        j=i;
+        while(i*j<2000000){
+            arr[i*j]=1;
+            j++;
+        }
+        i++;
+        while(arr[i]==1)
+            i++;
+    }
+    suma=0;
+    for(i=1;i<=2000000;i++)
+        if(arr[i]==0)
+            suma=suma+i;
+    i=0;
+     cout <<suma<<endl;
+    getch();
+    return 0;
+}
