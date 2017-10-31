d230c7c2dc3d5f1d8262fb1c30d3ec4fb7976f66
Rupraj <s521059@mail.nwmissouri.edu>
2017-10-31 13:01:08 -0500

    Add files via upload

---
+++ EulerPro4/cmake-build-debug/CMakeFiles/EulerPro4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EulerPro4.dir/main.cpp.obj"
+  "EulerPro4.pdb"
+  "EulerPro4.exe"
+  "EulerPro4.exe.manifest"
+  "libEulerPro4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EulerPro4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
