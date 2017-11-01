5ab5b27e9c8b71f702ba9959a3335370110891ea
Rupraj <s521059@mail.nwmissouri.edu>
2017-11-01 10:50:14 -0500

    Add files via upload

---
+++ EulerP8/cmake-build-debug/CMakeFiles/EulerP8.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EulerP8.dir/main.cpp.obj"
+  "EulerP8.pdb"
+  "EulerP8.exe"
+  "EulerP8.exe.manifest"
+  "libEulerP8.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EulerP8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
