ffca9ac4173420eba9a507440b85df38954773de
Rupraj <s521059@mail.nwmissouri.edu>
2017-11-01 18:25:35 -0500

    Add files via upload

---
+++ EulerP13/cmake-build-debug/CMakeFiles/EulerP13.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EulerP13.dir/main.cpp.obj"
+  "EulerP13.pdb"
+  "EulerP13.exe"
+  "EulerP13.exe.manifest"
+  "libEulerP13.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EulerP13.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ EulerP13/main.cpp
@@ -0,0 +1,42 @@
+#include <cstdint>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <unistd.h>
+
+template <typename T> T too_big( const char* num )
+{
+    std::stringstream ss(num);
+    T ret;
+    return ss >> ret ? ret : 0;
+}
+
+std::string large_sum()
+{
+    static const char* file_name = "/Users/s521059/CLionProjects/Euler Project/EulerP13/gg.txt";
+
+    uint64_t sum = 0;
+
+    std::ifstream fin(file_name);
+
+    if( !fin.is_open() ){
+        std::cerr << "Failed to open file: " << file_name << std::endl;
+        return "-1";
+    }
+
+    for( std::string line ; std::getline(fin,line);){
+        // We only care about first 11 (10 + 1) digits
+        // We do not need 'Big Int' support
+        sum += too_big<uint64_t>(line.substr(0,11).c_str());
+    }
+    return std::to_string(sum).substr(0,10);
+}
+
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "Answer: " << large_sum() << std::endl;
+}
