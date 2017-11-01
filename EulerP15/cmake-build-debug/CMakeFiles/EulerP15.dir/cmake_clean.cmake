c957be3d33ce2fff5e58f4301ea9263c87837149
Rupraj <s521059@mail.nwmissouri.edu>
2017-11-01 18:51:18 -0500

    Add files via upload

---
+++ EulerP15/cmake-build-debug/CMakeFiles/EulerP15.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EulerP15.dir/main.cpp.obj"
+  "EulerP15.pdb"
+  "EulerP15.exe"
+  "EulerP15.exe.manifest"
+  "libEulerP15.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EulerP15.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
